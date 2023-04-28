5941c87eede58bcb5e79db377dc313e0518c5b0a
Iseoluwa <iseoluwagbotemidaniel@gmail.com>
2023-04-28 05:37:23 -0400

    awesome, nitrogen, fontconfig, wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require('wezterm')
+
+local config = {
+    hide_tab_bar_if_only_one_tab = true,
+    use_fancy_tab_bar = false,
+    window_background_opacity = 0.75,
+    font_size = 10.0,
+    color_scheme = "Catppuccin Mocha",
+}
+
+return config
