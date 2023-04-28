5e7f3789bd41c3a27a50d4827b27091335fa6360
Iseoluwa <iseoluwagbotemidaniel@gmail.com>
2023-04-28 09:57:51 -0400

    wallpapers, rofi

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local config = {
     use_fancy_tab_bar = false,
     window_background_opacity = 0.75,
     font_size = 10.0,
+    -- color_scheme = "MaterialOcean",
     color_scheme = "Catppuccin Mocha",
 }
 
